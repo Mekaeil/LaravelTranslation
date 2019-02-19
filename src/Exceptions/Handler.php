c8f6322422ebe523c7d696047bd89f27201fd1c1
mekaeil <maikel1370@gmail.com>
2019-02-19 22:07:46 +0330

    edit ecryption in handle exception

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -4,7 +4,10 @@ namespace Mekaeil\LaravelTranslation\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
- use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Crypt;
+
 
 class Handler extends ExceptionHandler
 {
@@ -23,13 +26,33 @@ class Handler extends ExceptionHandler
         ///////////////////////////////////////////////////////////////////////////////
         if ($exception instanceof NotFoundHttpException && $_SERVER['REQUEST_METHOD'] == 'GET')
         {
-            $locale         = app('encrypter')->decrypt(request()->cookie('language'), false) ?? app()->getLocale();
+            $locale = app()->getLocale();
+            if (Cookie::has('language'))
+            {
+                $locale = Crypt::decryptString(Cookie::get('language'));
+            }
+
             $currentURL     = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+//            /// CHECK WHEN WE ARE IN HOME PAGE
+//            //////////////////////////////////////////////////////////////////////////////
+//            $target     = parse_url($currentURL, PHP_URL_SCHEME) . "://";
+//            $target    .= parse_url($currentURL, PHP_URL_HOST);
+//            $target    .= parse_url($currentURL, PHP_URL_PORT) ? ':' : '';
+//            $target    .= parse_url($currentURL, PHP_URL_PORT);
+//            $start      = strlen($target) + strlen($locale) + 1;
+
             $ifLocaleExist  = strpos($currentURL,'/'.$locale.'/');
 
             if ($ifLocaleExist)
             {
                 $newUrl   =  str_replace('/'.$locale.'/','/',$currentURL);
+                return redirect()->to($newUrl);
+            }
+            elseif(strpos($currentURL,'/'.$locale))
+            {
+                dd('ss');
+                $newUrl   =  str_replace('/'.$locale,'/',$currentURL);
                 return redirect()->to($newUrl);
             }
 
