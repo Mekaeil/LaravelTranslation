0679607dae59f6090909a99eda0d98616af6aed0
mekaeil <maikel1370@gmail.com>
2019-02-20 12:30:49 +0330

    edit exception handler encryption

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -34,14 +34,6 @@ class Handler extends ExceptionHandler
 
             $currentURL     = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-//            /// CHECK WHEN WE ARE IN HOME PAGE
-//            //////////////////////////////////////////////////////////////////////////////
-//            $target     = parse_url($currentURL, PHP_URL_SCHEME) . "://";
-//            $target    .= parse_url($currentURL, PHP_URL_HOST);
-//            $target    .= parse_url($currentURL, PHP_URL_PORT) ? ':' : '';
-//            $target    .= parse_url($currentURL, PHP_URL_PORT);
-//            $start      = strlen($target) + strlen($locale) + 1;
-
             $ifLocaleExist  = strpos($currentURL,'/'.$locale.'/');
 
             if ($ifLocaleExist)
@@ -49,16 +41,10 @@ class Handler extends ExceptionHandler
                 $newUrl   =  str_replace('/'.$locale.'/','/',$currentURL);
                 return redirect()->to($newUrl);
             }
-            elseif(strpos($currentURL,'/'.$locale))
-            {
-                dd('ss');
-                $newUrl   =  str_replace('/'.$locale,'/',$currentURL);
-                return redirect()->to($newUrl);
-            }
 
         }
         ///////////////////////////////////////////////////////////////////////////////
-
         return parent::render($request, $exception);
+
     }
 }
